767be7181c52ad508748b12aaf83c434e74c8ada
Cinthia <cinthia.tengan@gmail.com>
2021-01-20 22:21:49 -0300

    end of video 3

--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserLogin } from '../model/UserLogin';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -8,12 +10,40 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class CadastrarComponent implements OnInit {
 
-  userLogin: UserLogin = new UserLogin
+  user: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(){
     window.scroll(0,0)
   }
+
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+  cadastrar(){
+    this.user.tipo= this.tipoUsuario
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas.')
+    } else{
+      this.authService.cadastrar(this.user).subscribe((resp:User)=> {
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+
+    }
+  }
+
+
 
 }
