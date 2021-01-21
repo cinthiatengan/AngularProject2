87ff2d1731565daaa33872fb386b1ef3d6adf99e
Cinthia <cinthia.tengan@gmail.com>
2021-01-20 23:02:05 -0300

    last update from 20-01-2021

--- blogPessoal/src/app/entrar/entrar.component.ts
+++ blogPessoal/src/app/entrar/entrar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { AuthService } from '../service/auth.service';
 
@@ -23,6 +24,19 @@ export class EntrarComponent implements OnInit {
   entrar(){
     this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
       this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+
+      //console.log(environment.token)
+      //console.log(environment.nome)
+      //console.log(environment.foto)
+      //console.log(environment.id)
+
+      this.userLogin.foto
+
       this.router.navigate(['inicio'])
     }, erro => {
       if(erro.status == 500){
